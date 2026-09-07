7d41323b4b872c68fea828000ce3ddd73a4a933b
abhedwig-cell <abhedwig@gmail.com>
2026-09-07 07:53:38 +0200

    F-CI08: test irrigation schedule rollback

--- tests/fci/test_fci08_legacy_trial_capsule.f90
+++ tests/fci/test_fci08_legacy_trial_capsule.f90
@@ -5,7 +5,7 @@ program test_fci08_legacy_trial_capsule
   use MOD_meteo, only: meteo_rec
   use MOD_SoilTemperature, only: ipos_qtop,ipos_tetop,ipos_tebot
   use MOD_Solute, only: imdectot,imsqprec,imqsol,sbaldev,samini,isqtop
-  use MOD_irrigation, only: dayfix,nirri,qssdi
+  use MOD_irrigation, only: schedule,dayfix,nirri,qssdi
   use MOD_integral, only: inqrot,inq,inqdra,cgrai,cqdrain
   use mod_b1_10_legacy_trial_capsule, only: b1_10_legacy_trial_capsule_t, &
        capture_b1_10_legacy_trial_capsule, restore_b1_10_legacy_trial_capsule
@@ -15,7 +15,7 @@ program test_fci08_legacy_trial_capsule
 
   dt=0.125_real64; t1900=40000.5_real64; fldecdt=.true.; meteo_rec=31
   ipos_qtop=11; ipos_tetop=12; ipos_tebot=13
-  dayfix=6; nirri=2
+  schedule=1; dayfix=6; nirri=2
   do i=1,macp
     qssdi(i)=real(i,real64)*1.e-5_real64
     inqrot(i)=real(i,real64)*2.e-5_real64
@@ -30,7 +30,7 @@ program test_fci08_legacy_trial_capsule
   call capture_b1_10_legacy_trial_capsule(c)
 
   dt=-9; t1900=-9; fldecdt=.false.; meteo_rec=-9
-  ipos_qtop=-9; ipos_tetop=-9; ipos_tebot=-9; dayfix=-9; nirri=-9
+  ipos_qtop=-9; ipos_tetop=-9; ipos_tebot=-9; schedule=-9; dayfix=-9; nirri=-9
   qssdi=-9; inqrot=-9; inq=-9; inqdra=-9; cgrai=-9; cqdrain=-9
   imdectot=-9; imsqprec=-9; imqsol=-9; sbaldev=-9; samini=-9; isqtop=-9
 
@@ -39,7 +39,7 @@ program test_fci08_legacy_trial_capsule
   call chk(dt,0.125_real64,'dt'); call chk(t1900,40000.5_real64,'t1900')
   if(.not.fldecdt .or. meteo_rec/=31) error stop 'legacy rollback control/forcing'
   if(ipos_qtop/=11 .or. ipos_tetop/=12 .or. ipos_tebot/=13) error stop 'thermal forcing cursor rollback'
-  if(dayfix/=6 .or. nirri/=2) error stop 'irrigation rollback copy'
+  if(schedule/=1 .or. dayfix/=6 .or. nirri/=2) error stop 'irrigation config/process rollback copy'
   call chk(qssdi(macp),real(macp,real64)*1.e-5_real64,'qssdi')
   call chk(inqrot(macp),real(macp,real64)*2.e-5_real64,'inqrot')
   call chk(inq(macp+1),real(macp+1,real64)*3.e-5_real64,'inq')
