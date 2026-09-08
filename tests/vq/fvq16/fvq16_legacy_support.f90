a589b7a8e9032eda1b0056103ac031b75489c8bc
abhedwig-cell <abhedwig@gmail.com>
2026-09-08 11:07:31 +0200

    F-VQ16: fix strict Fortran legacy error shim

--- tests/vq/fvq16/fvq16_legacy_support.f90
+++ tests/vq/fvq16/fvq16_legacy_support.f90
@@ -6,5 +6,6 @@ end module MOD_swap_base
 subroutine swap_error(routine_name, message)
   implicit none
   character(len=*), intent(in) :: routine_name, message
-  error stop trim(routine_name) // ': ' // trim(message)
+  write (*,'(A)') trim(routine_name) // ': ' // trim(message)
+  error stop 1
 end subroutine swap_error
