1840414a6304e8d2759b31d21ab200e399f8e3e0
abhedwig-cell <abhedwig@gmail.com>
2026-09-10 09:39:37 +0200

    feat(F-PM08C1): add normalized tabulated drainage response candidate

---
+++ src/process/mod_drainage_tabulated_response.f90
@@ -0,0 +1,148 @@
+module mod_drainage_tabulated_response
+  use, intrinsic :: iso_fortran_env, only: real64
+  use, intrinsic :: ieee_arithmetic, only: ieee_is_finite
+  use mod_process_hydraulic_view, only: process_hydraulic_view_t
+  implicit none
+  private
+
+  integer, parameter, public :: DRAIN_TAB_OK = 0
+  integer, parameter, public :: DRAIN_TAB_INVALID_PARAMETERS = 1
+  integer, parameter, public :: DRAIN_TAB_INVALID_HYDRAULIC_VIEW = 2
+
+  type, public :: drainage_tabulated_parameters_t
+    real(real64), allocatable :: groundwater_depth(:)
+    real(real64), allocatable :: signed_exchange_rate(:)
+  end type drainage_tabulated_parameters_t
+
+  type, public :: drainage_tabulated_result_t
+    real(real64) :: signed_soil_to_drain_rate = 0.0_real64
+    logical :: derivative_defined = .false.
+    real(real64) :: dq_dgroundwater_level = 0.0_real64
+  end type drainage_tabulated_result_t
+
+  type, public :: drainage_tabulated_diagnostics_t
+    integer :: status = DRAIN_TAB_OK
+    logical :: evaluated = .false.
+    logical :: clamped_lower = .false.
+    logical :: clamped_upper = .false.
+    logical :: at_table_knot = .false.
+    integer :: segment_index = 0
+    real(real64) :: groundwater_level = 0.0_real64
+    real(real64) :: groundwater_depth = 0.0_real64
+    logical :: mass_is_authoritative_external_transfer = .true.
+    logical :: persistent_process_state = .false.
+  end type drainage_tabulated_diagnostics_t
+
+  public :: evaluate_tabulated_drainage_response
+
+contains
+
+  subroutine evaluate_tabulated_drainage_response(parameters, hydraulic_view, response, diagnostics)
+    type(drainage_tabulated_parameters_t), intent(in) :: parameters
+    type(process_hydraulic_view_t), intent(in) :: hydraulic_view
+    type(drainage_tabulated_result_t), intent(out) :: response
+    type(drainage_tabulated_diagnostics_t), intent(out) :: diagnostics
+
+    real(real64) :: depth, slope
+    integer :: i, n
+
+    response = drainage_tabulated_result_t()
+    diagnostics = drainage_tabulated_diagnostics_t()
+
+    if (.not. valid_parameters(parameters)) then
+      diagnostics%status = DRAIN_TAB_INVALID_PARAMETERS
+      return
+    end if
+
+    if (.not. ieee_is_finite(hydraulic_view%groundwater_level)) then
+      diagnostics%status = DRAIN_TAB_INVALID_HYDRAULIC_VIEW
+      return
+    end if
+
+    diagnostics%evaluated = .true.
+    diagnostics%groundwater_level = hydraulic_view%groundwater_level
+    depth = abs(hydraulic_view%groundwater_level)
+    diagnostics%groundwater_depth = depth
+    n = size(parameters%groundwater_depth)
+
+    if (n == 1) then
+      response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(1)
+      response%derivative_defined = .true.
+      response%dq_dgroundwater_level = 0.0_real64
+      diagnostics%clamped_lower = .true.
+      diagnostics%clamped_upper = .true.
+      return
+    end if
+
+    if (depth < parameters%groundwater_depth(1)) then
+      response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(1)
+      response%derivative_defined = .true.
+      response%dq_dgroundwater_level = 0.0_real64
+      diagnostics%clamped_lower = .true.
+      return
+    end if
+
+    if (depth > parameters%groundwater_depth(n)) then
+      response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(n)
+      response%derivative_defined = .true.
+      response%dq_dgroundwater_level = 0.0_real64
+      diagnostics%clamped_upper = .true.
+      return
+    end if
+
+    if (.not. (depth > parameters%groundwater_depth(1))) then
+      response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(1)
+      diagnostics%at_table_knot = .true.
+      return
+    end if
+
+    do i = 2, n
+      if (depth < parameters%groundwater_depth(i)) then
+        slope = (parameters%signed_exchange_rate(i) - parameters%signed_exchange_rate(i-1)) / &
+             (parameters%groundwater_depth(i) - parameters%groundwater_depth(i-1))
+        response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(i-1) + &
+             (depth - parameters%groundwater_depth(i-1)) * slope
+        if (hydraulic_view%groundwater_level > 0.0_real64) then
+          response%dq_dgroundwater_level = slope
+        else
+          response%dq_dgroundwater_level = -slope
+        end if
+        response%derivative_defined = .true.
+        diagnostics%segment_index = i - 1
+        return
+      end if
+
+      if (.not. (depth > parameters%groundwater_depth(i))) then
+        response%signed_soil_to_drain_rate = parameters%signed_exchange_rate(i)
+        diagnostics%at_table_knot = .true.
+        diagnostics%segment_index = i - 1
+        return
+      end if
+    end do
+
+    diagnostics%status = DRAIN_TAB_INVALID_PARAMETERS
+    diagnostics%evaluated = .false.
+    response = drainage_tabulated_result_t()
+  end subroutine evaluate_tabulated_drainage_response
+
+  logical function valid_parameters(parameters) result(valid)
+    type(drainage_tabulated_parameters_t), intent(in) :: parameters
+    integer :: i, n
+
+    valid = .false.
+    if (.not. allocated(parameters%groundwater_depth)) return
+    if (.not. allocated(parameters%signed_exchange_rate)) return
+    n = size(parameters%groundwater_depth)
+    if (n < 1) return
+    if (size(parameters%signed_exchange_rate) /= n) return
+    if (any(.not. ieee_is_finite(parameters%groundwater_depth))) return
+    if (any(.not. ieee_is_finite(parameters%signed_exchange_rate))) return
+    if (any(parameters%groundwater_depth < 0.0_real64)) return
+
+    do i = 2, n
+      if (.not. (parameters%groundwater_depth(i) > parameters%groundwater_depth(i-1))) return
+    end do
+    valid = .true.
+  end function valid_parameters
+
+end module mod_drainage_tabulated_response
