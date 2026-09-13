f2bceedb7564f46c04e7fa5370f2fa924cdf4735
abhedwig-cell <abhedwig@gmail.com>
2026-09-13 22:10:32 +0200

    test(F-VQ71): add independent split-publication compile hard negative

---
+++ tests/fvq/fvq71_forbidden_split_surface_publication.f90
@@ -0,0 +1,7 @@
+program fvq71_forbidden_split_surface_publication
+  use mod_fmr_surface_evaporation_accepted_publication, only: &
+       fmr_prepare_surface_evaporation_publication, &
+       fmr_finalize_surface_evaporation_publication
+  implicit none
+  print *, 'forbidden historical split accepted-publication API is visible'
+end program fvq71_forbidden_split_surface_publication
