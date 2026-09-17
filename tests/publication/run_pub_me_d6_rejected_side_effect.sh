5fdf9f904b3ea70f0c116621ab2478bd47317cd1
abhedwig-cell <abhedwig@gmail.com>
2026-09-18 01:59:21 +0200

    test(PUB-ME D6): include directional publication dependency

--- tests/publication/run_pub_me_d6_rejected_side_effect.sh
+++ tests/publication/run_pub_me_d6_rejected_side_effect.sh
@@ -21,6 +21,7 @@ MODULE_SRC=(
   tests/fsi/fsi04_real_headcalc_stubs.f90
   src/solver/mod_soil_water_accepted_step_direction_contract.f90
   src/transaction/mod_accepted_trajectory_directional_sensitivity.f90
+  src/transaction/mod_accepted_trajectory_directional_publication.f90
   src/runtime/mod_a23bu_worker_execution_context.f90
   src/transaction/mod_transaction_reference.f90
   src/transaction/mod_fkt_temporal_indicator_history.f90
