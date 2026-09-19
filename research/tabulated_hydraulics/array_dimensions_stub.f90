0c6e50c3ea85045ad4c88dcc80858ff134e4a10f
abhedwig-cell <abhedwig@gmail.com>
2026-09-19 17:02:21 +0200

    TAB-HYD audit: add isolated array-dimension stub

---
+++ research/tabulated_hydraulics/array_dimensions_stub.f90
@@ -0,0 +1,6 @@
+module swap_array_dimensions
+  implicit none
+  integer, parameter :: macp = 1
+  integer, parameter :: matab = 1000
+  integer, parameter :: matabentries = 50005
+end module swap_array_dimensions
