58efd1b689d15561a4195aa2d8bc4eb82d7c1d3d
abhedwig-cell <abhedwig@gmail.com>
2026-09-19 17:05:41 +0200

    F-TAB01: add isolated legacy oracle dimension stub

---
+++ tests/tabulated/reference/swap_array_dimensions_stub.f90
@@ -0,0 +1,6 @@
+module swap_array_dimensions
+  implicit none
+  integer, parameter :: MACP = 2
+  integer, parameter :: MATAB = 1000
+  integer, parameter :: MATABENTRIES = 50005
+end module swap_array_dimensions
