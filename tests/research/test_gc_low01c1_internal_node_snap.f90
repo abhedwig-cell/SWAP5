a9c739e8b305d2677a80ab8ce53e05eab0ef0c4a
abhedwig-cell <abhedwig@gmail.com>
2026-09-21 11:15:07 +0200

    fix(gc-low01): bind C1 top-mode constant to state contract

--- tests/research/test_gc_low01c1_internal_node_snap.f90
+++ tests/research/test_gc_low01c1_internal_node_snap.f90
@@ -3,10 +3,9 @@ program test_gc_low01c1_internal_node_snap
   use, intrinsic :: iso_fortran_env, only: int64, real64
   use mod_a23bu_worker_execution_context, only: a23bu_worker_context_t, a23bu_solver_history_t
   use mod_reference_richards_workspace, only: reference_richards_workspace_t
-  use mod_reference_richards_state_binding, only: reference_richards_state_binding_t
+  use mod_reference_richards_state_binding, only: reference_richards_state_binding_t, FSI_TOP_MODE_EXPLICIT_FLUX
   use mod_soil_water_solver_contract, only: hydraulic_evaluation_context_t, soil_water_boundary_conditions_t, &
-       soil_water_numerical_config_t, soil_water_physical_config_t, soil_water_parameter_set_t, &
-       FSI_TOP_MODE_EXPLICIT_FLUX
+       soil_water_numerical_config_t, soil_water_physical_config_t, soil_water_parameter_set_t
   use mod_b110_default_mvg_provider, only: b110_default_mvg_parameters_t, b110_default_mvg_provider_t, &
        initialize_b110_default_mvg_parameters, bind_b110_default_mvg_provider
   use mod_b110_source_sink_provider, only: b110_source_sink_provider_t, bind_b110_source_sink_provider
