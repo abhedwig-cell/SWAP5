a50a5c7a8f2437f365a108894ce1e67627b52ca0
abhedwig-cell <abhedwig@gmail.com>
2026-09-24 20:07:24 +0200

    F-AHL04A: add nonlinear Richards provider replay

---
+++ research/ahl/test_ahl04a_richards_provider_replay.f90
@@ -0,0 +1,153 @@
+program test_ahl04a_richards_provider_replay
+  use, intrinsic :: iso_fortran_env, only: int64, real64
+  use MOD_grid, only: numnod, z, dz, disnod
+  use mod_soil_water_solver_contract, only: soil_water_parameter_set_t, soil_water_physical_state_t, &
+       soil_water_solve_request_t, soil_water_solve_result_t, SW_SOLVE_CONVERGED
+  use mod_reference_richards_legacy_binding, only: reference_richards_legacy_solver_t, &
+       reference_richards_legacy_workspace_t
+  use mod_reference_richards_state_binding, only: FSI_TOP_MODE_EXPLICIT_FLUX
+  use mod_b110_default_mvg_provider, only: b110_default_mvg_parameters_t, b110_default_mvg_provider_t, &
+       initialize_b110_default_mvg_parameters, bind_b110_default_mvg_provider
+  use mod_b110_source_sink_provider, only: b110_source_sink_provider_t, bind_b110_source_sink_provider
+  use mod_fmr04_fixed_top_provider, only: fmr04_fixed_flux_top_provider_t
+  use mod_ahl04_hybrid_lookup_provider, only: ahl04_hybrid_lookup_provider_t, &
+       bind_ahl04_hybrid_lookup_provider
+  implicit none
+
+  real(real64), parameter :: total_dt=0.25_real64, mass_gate=1.0e-12_real64
+  real(real64), parameter :: h0=-75.0_real64, hbot=-50.0_real64
+  type(soil_water_parameter_set_t), target :: parameters
+  type(b110_default_mvg_parameters_t), target :: hp
+  type(b110_default_mvg_provider_t), target :: analytical
+  type(ahl04_hybrid_lookup_provider_t), target :: lookup
+  type(b110_source_sink_provider_t), target :: source_sink
+  type(fmr04_fixed_flux_top_provider_t), target :: top_provider
+  type(soil_water_physical_state_t) :: initial_state
+  type(soil_water_solve_request_t) :: request
+  type(soil_water_solve_result_t) :: reference_result, candidate_result
+  type(reference_richards_legacy_solver_t) :: solver_ref, solver_candidate
+  type(reference_richards_legacy_workspace_t) :: workspace_ref, workspace_candidate
+  real(real64), allocatable, target :: drainage(:,:), subsurface(:), root_sink(:)
+  real(real64), allocatable :: cofgen(:,:)
+  real(real64) :: heads(numnod), water(numnod), conductivity(numnod), capacity(numnod), dkdh(numnod)
+  real(real64) :: k0
+  character(len=512) :: table_path, label
+  logical :: valid
+  integer :: k
+
+  if (command_argument_count() /= 2) error stop 'usage: test TABLE_PATH LABEL'
+  call get_command_argument(1,table_path)
+  call get_command_argument(2,label)
+
+  parameters%parameter_set_id=404001_int64
+  parameters%active_nodes=numnod
+  allocate(parameters%z(numnod),parameters%dz(numnod),parameters%node_distance(numnod))
+  parameters%z=z; parameters%dz=dz; parameters%node_distance=disnod(1:numnod)
+
+  allocate(cofgen(24,numnod)); cofgen=0.0_real64
+  do k=1,numnod
+    cofgen(1,k)=0.032_real64; cofgen(2,k)=0.423_real64; cofgen(3,k)=4.75_real64
+    cofgen(4,k)=0.0135_real64; cofgen(5,k)=0.365_real64; cofgen(6,k)=1.455_real64
+    cofgen(7,k)=1.0_real64-1.0_real64/cofgen(6,k); cofgen(8,k)=cofgen(4,k)
+    cofgen(9,k)=0.0_real64; cofgen(10,k)=cofgen(3,k); cofgen(11,k)=0.999_real64
+    cofgen(12,k)=0.99_real64*cofgen(3,k); cofgen(22,k)=-1.0e6_real64; cofgen(23,k)=1.0e-12_real64
+  end do
+  call initialize_b110_default_mvg_parameters(hp,cofgen)
+  call bind_b110_default_mvg_provider(analytical,hp,total_dt)
+
+  heads=h0
+  call analytical%evaluate(heads,water,conductivity,capacity,dkdh)
+  k0=conductivity(1)
+
+  initial_state%active_nodes=numnod
+  allocate(initial_state%pressure_head(numnod),initial_state%water_content(numnod))
+  initial_state%pressure_head=heads; initial_state%water_content=water
+  initial_state%ponding_depth=0.0_real64; initial_state%groundwater_level=-2.0_real64
+
+  allocate(drainage(1,numnod),subsurface(numnod),root_sink(numnod))
+  drainage=0.0_real64; subsurface=0.0_real64; root_sink=0.0_real64
+  call bind_b110_source_sink_provider(source_sink,drainage,subsurface,root_sink)
+
+  request=soil_water_solve_request_t()
+  request%parameters=>parameters
+  request%base_state=initial_state
+  request%step_duration=total_dt
+  request%boundary%top_mode=FSI_TOP_MODE_EXPLICIT_FLUX
+  request%boundary%bottom_mode=5
+  request%boundary%top_flux=-k0
+  request%boundary%top_head=h0
+  request%boundary%bottom_flux=0.0_real64
+  request%boundary%bottom_head=hbot
+  request%physical%macropore_active=.false.
+  request%numerical%max_iterations=8
+  request%numerical%max_backtracking=4
+  request%numerical%conductivity_implicit_mode=0
+  request%numerical%conductivity_mean_method=1
+  request%numerical%min_step_duration=1.0e-6_real64
+  request%numerical%compartment_balance_tolerance=mass_gate
+  request%numerical%total_balance_tolerance=mass_gate
+  request%numerical%head_abs_tolerance=1.0e-12_real64
+  request%numerical%head_rel_tolerance=1.0e-12_real64
+  request%numerical%ponding_tolerance=1.0e-12_real64
+  request%evaluation%source_sink=>source_sink
+  request%evaluation%top_boundary=>top_provider
+
+  request%evaluation%constitutive=>analytical
+  call solver_ref%solve(request,workspace_ref,reference_result)
+  call require(reference_result%status==SW_SOLVE_CONVERGED,'analytical solve converged')
+
+  call bind_ahl04_hybrid_lookup_provider(lookup,hp,total_dt,trim(table_path),valid)
+  call require(valid,'lookup provider bound')
+  request%evaluation%constitutive=>lookup
+  request%base_state=initial_state
+  call solver_candidate%solve(request,workspace_candidate,candidate_result)
+  call require(candidate_result%status==SW_SOLVE_CONVERGED,'lookup solve converged')
+
+  call report_and_gate(trim(label),reference_result,candidate_result)
+
+contains
+
+  subroutine report_and_gate(name,ref,cand)
+    character(len=*), intent(in) :: name
+    type(soil_water_solve_result_t), intent(in) :: ref,cand
+    real(real64) :: dh,dw,dtf,dbf,mass
+    integer :: diter, dbt
+    dh=maxval(abs(cand%candidate_state%pressure_head-ref%candidate_state%pressure_head))
+    dw=maxval(abs(cand%candidate_state%water_content-ref%candidate_state%water_content))
+    dtf=abs(cand%top_flux-ref%top_flux)
+    dbf=abs(cand%bottom_flux-ref%bottom_flux)
+    diter=abs(cand%diagnostics%nonlinear_iterations-ref%diagnostics%nonlinear_iterations)
+    dbt=abs(cand%diagnostics%backtracking_attempts-ref%diagnostics%backtracking_attempts)
+    if (cand%integrated_mass_balance_residual_available) then
+      mass=abs(cand%integrated_mass_balance_residual_cm)
+    else
+      mass=abs(cand%unrounded_mass_balance_residual)
+    end if
+
+    write(*,'(A,1X,A,1X,A,ES14.6,1X,A,ES14.6,1X,A,ES14.6,1X,A,ES14.6,1X,A,ES14.6)') &
+         'AHL04A',trim(name),'MAX_DH_CM=',dh,'MAX_DTHETA=',dw,'DTOP=',dtf,'DBOTTOM=',dbf,'MASS=',mass
+    write(*,'(A,1X,A,1X,A,I0,1X,A,I0,1X,A,I0,1X,A,I0)') &
+         'AHL04A_DIAG',trim(name),'REF_ITER=',ref%diagnostics%nonlinear_iterations, &
+         'CAND_ITER=',cand%diagnostics%nonlinear_iterations,'REF_BACKTRACK=',ref%diagnostics%backtracking_attempts, &
+         'CAND_BACKTRACK=',cand%diagnostics%backtracking_attempts
+
+    call require(dh<=0.05_real64,'pressure-head gate')
+    call require(dw<=1.0e-4_real64,'water-content gate')
+    call require(dtf<=1.0e-5_real64,'top-flux gate')
+    call require(dbf<=1.0e-5_real64,'bottom-flux gate')
+    call require(mass<=mass_gate,'mass gate')
+    call require(diter<=2,'iteration-count gate')
+    call require(dbt<=2,'backtracking-count gate')
+    write(*,'(A,1X,A,1X,A)') 'AHL04A',trim(name),'PASS'
+  end subroutine report_and_gate
+
+  subroutine require(condition,message)
+    logical,intent(in)::condition
+    character(len=*),intent(in)::message
+    if(.not.condition) then
+      write(*,'(A,1X,A)') 'AHL04A_FAIL',trim(message)
+      error stop 1
+    end if
+  end subroutine require
+
+end program test_ahl04a_richards_provider_replay
