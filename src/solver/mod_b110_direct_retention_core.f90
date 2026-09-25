ff7de2171e1de71ac8c70b15fda1ad37b9b82e8b
abhedwig-cell <abhedwig@gmail.com>
2026-09-25 17:35:08 +0200

    F-AHL49: create direct-retention core module

---
+++ src/solver/mod_b110_direct_retention_core.f90
@@ -0,0 +1,7 @@
+module mod_b110_direct_retention_core
+  use, intrinsic :: iso_fortran_env, only: int64, real64
+  implicit none
+  private
+  integer, parameter, public :: B110_DIRECT_RETENTION_INTERVALS_PER_DECADE = 64
+contains
+end module mod_b110_direct_retention_core
