5620c145dbf0cca1f9ea99456c25285f26ac007e
abhedwig-cell <abhedwig@gmail.com>
2026-09-26 13:39:04 +0200

    F-PE-APPROX03: remove duplicate certificate link object

--- tests/fpe/run_fpe_approx03_certificate_discovery.sh
+++ tests/fpe/run_fpe_approx03_certificate_discovery.sh
@@ -134,7 +134,6 @@ MODULE_SRC=(
   src/solver/mod_rossfast_d3r_soil_water_solver.f90
   src/runtime/mod_fmr_rossfast_solver_selection_binding.f90
   src/runtime/mod_fmr_serialized_reference_backend.f90
-  src/runtime/mod_fmr_accepted_commit_receipt.f90
   src/runtime/mod_fmr_serialized_multiswap_runtime.f90
 )
 objects=()
