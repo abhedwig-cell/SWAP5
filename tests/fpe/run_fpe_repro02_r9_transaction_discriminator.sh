dc7481d53e71eac55b3525346aa4e71970e58e5e
abhedwig-cell <abhedwig@gmail.com>
2026-09-26 19:08:09 +0200

    F-PE-REPRO02: repair R9 harness parsing

--- tests/fpe/run_fpe_repro02_r9_transaction_discriminator.sh
+++ tests/fpe/run_fpe_repro02_r9_transaction_discriminator.sh
@@ -56,7 +56,7 @@ src=src.replace(
 "  public :: fgc44_predictor_run_diagnostics_c\n"
 "  public :: fgc44_approx04_configure_case_c, fgc44_approx04_p1b_state_c, fgc44_approx04_predictor_q_c\n"
 "  public :: fgc44_repro02_observation_c, fgc44_repro02_floor_c\n",1)
-needle="contains\\n\\n"
+needle="""contains\n\n"""
 insert="""contains
 
   integer(c_int) function fgc44_repro02_floor_c(head_m,floor_status,sample_valid,physical_advances,nonlinear, &
